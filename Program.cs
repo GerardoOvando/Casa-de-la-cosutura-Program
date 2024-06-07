fc2bfa9294eaa0e01edd9bde35e780176d290890
Gerardo Ovando <gerardo.o.rodriguez@hotmail.com>
2024-06-06 22:42:39 -0600

    Iniciando el proyecto

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PracticandoTodo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Casa de la Costura Program                        ";
+            Console.Write("Casa de la Costura                   ");
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+
+}
