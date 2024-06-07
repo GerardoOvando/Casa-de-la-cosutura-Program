e30dbbaba5a84d07ddee3b6925ccaa69c8d663ee
Gerardo Ovando <gerardo.o.rodriguez@hotmail.com>
2024-06-06 23:13:26 -0600

    Agregando la clase Cliente

--- Program.cs
+++ Program.cs
@@ -9,7 +9,22 @@ namespace PracticandoTodo
         {
             Console.Title = "Casa de la Costura Program                        ";
             Console.Write("Casa de la Costura                   ");
-            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(DateTime.Now + "\n");
+
+            Console.WriteLine("¿Qué deseas realizar? \n");
+            Console.WriteLine("1) Registrar nuevo cliente o pedido.\n" +
+                "2) Revisar calendario.\n" +
+                "3) Revisar base de datos.");
+            int opcion = int.Parse(Console.ReadLine());
+            switch (opcion)
+            {
+                case 1:
+
+                    break;
+            }
+
+
+
         }
     }
 
